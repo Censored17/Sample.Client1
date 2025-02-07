e8346afec6e460a08057c5ac43cb0cecee00bed0
Jaimher Ayson <aysonjam17@gmail.com>
2025-02-07 10:09:24 +0800

    pro

--- SampleClient2/Pages/Class.cs
+++ SampleClient2/Pages/Class.cs
@@ -1,6 +1,17 @@
-﻿namespace SampleClient2.Pages
+﻿using Microsoft.Extensions.Logging.Debug;
+
+namespace SampleClient2.Pages
 {
+    
     public class Class
     {
+        private readonly ILogger<Class> logger;
+
+        public Class(ILogger<Class> logger)
+        {
+            this.logger = logger;
+        }
+
+      
     }
 }
